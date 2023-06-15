a2dad004ad919245d500d979329e3f264857674b
Serhii Neliubov <84928517+JakeJenneth@users.noreply.github.com>
2023-06-15 21:01:34 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Product } from './components/Product';
+import { products } from './data/products';
+
+
+function App() {
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      {products.map(product => <Product product={product} key={product.id}/>)}
+    </div>
+  )
+}
+
+export default App;
